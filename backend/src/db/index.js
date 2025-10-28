664a8e3911fb9d50f6a8db4383704a6f98866d47
thenarain <kartiksrivastava19@gmail.com>
2025-10-28 05:59:44 +0530

    add ssl for Render PostgreSQL

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -10,6 +10,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
